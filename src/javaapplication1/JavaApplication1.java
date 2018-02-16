17c9504b6c894e8bc289d97027da81f579046087
AhmedSalahdin <36265289+AhmedSalahdin@users.noreply.github.com>
2018-02-16 13:33:47 +0200

    Add files via upload

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,12 @@
+package javaapplication1;
+import java.util.Scanner;
+public class JavaApplication1 {
+    public static void main(String[] args) {
+        all x= new all();
+        x.setLocationRelativeTo(null);
+        x.show();
+       
+        
+    }
+    
+}
---
+++ src/newpackage/NewClass.java
@@ -0,0 +1,15 @@
+package newpackage;
+
+import java.util.Scanner;
+
+public class NewClass {
+    
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        
+    }
+
+}
